098343c5478f19be897089e0aeffbd226e2b5cac
yangdepu <965093975@qq.com>
2017-05-18 12:14:08 +0800

    function
    js 函数学习

--- day2/hellojavascript.js
+++ day2/hellojavascript.js
@@ -1,1 +1,35 @@
 //函数的学习
+//1.1--函数的定义和调用
+//方法一
+function abss(x) {
+    if (x > 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+console.log(abss(-9));  //9
+//方法二
+var absss = function (x) {
+    if (x > 0) {
+        return x;
+    } else {
+        return -x;
+    }
+};
+console.log(absss(-12));  //12
+
+//调用函数传入的参数可以是任意个数的,对传入的参数进行检查
+function abs1(x) {
+    if (typeof (x) != 'number') {
+        throw 'Not a number';
+    }
+    if (x > 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+console.log(abs1('A'));  //抛出异常
+
+
