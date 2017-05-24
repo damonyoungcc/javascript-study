3dcbe58602a3db622b9f9cd741f47f275ada46cc
yangdepu <965093975@qq.com>
2017-05-24 18:06:29 +0800

    闭包

--- day4/hellojavascript.js
+++ day4/hellojavascript.js
@@ -2,62 +2,62 @@
 //filter()函数把传入的参数依次作用于每个元素，根据返回值是true还是false来决定是否保留元素
 
 //1.1 --练习，在一个数组中只保留偶数，删除奇数
-/*var arr = [1,2,3,4,5,6,7,8];
+var arr = [1,2,3,4,5,6,7,8];
 function foo(x){
     return x%2 ===0;
 }
-console.log(arr.filter(foo));  //[2, 4, 6, 8]*/
+console.log(arr.filter(foo));  //[2, 4, 6, 8]
 
 //1.2 --练习，把一个数组中的空字符串删掉
-/*var arr = ['A','B','',undefined,false,null];
+var arr = ['A','B','',undefined,false,null];
 console.log(arr.filter(function(x){
     return x != '';
-}));   //["A", "B", undefined, null]*/
+}));   //["A", "B", undefined, null]
 
 //filter()接收的回调函数可以接收多个参数
 //第一个参数表示Array的某个元素，第二个参数表示元素的位置（索引），第三个参数表示数组本身
-/*var arr = ['A', 'B', 'C'];
+var arr = ['A', 'B', 'C'];
 console.log(arr.filter(function (element, index, array) {
     return console.log(index);
-}));*/
+}));
 
 //利用filter()函数可以巧妙的去除数组中重复的元素
-/*var arr = ['apple', 'xiaomi', 'microsoft', 'IBM', 'apple', 'xiaomi'];
+var arr = ['apple', 'xiaomi', 'microsoft', 'IBM', 'apple', 'xiaomi'];
 console.log(arr.filter(function(ele,index,self){
     if(self.indexOf(ele) != index){
         return false;
     }else{
         return true;
     }
-}));*/
+}));
 //indexOf()方法只返回第一次出现要查找的元素的索引
 
 //利用filter()筛选出素数
 
 //test
 //just test值传递
-/*function foo(obj){
+function foo4(obj){
     obj.name = 'yang';
     obj = new Object();
     obj.name = 'rose';
 }
 var ob = new Object();
-foo(ob);
-console.log(ob.name);  //yang*/
+foo4(ob);
+console.log(ob.name);  //yang
 
 //2.0 =======================================================
 //2.0 =======================================================
 //2.1 --闭包
-/*function sum(arr){
+function sum(arr){
     return arr.reduce(function(x,y){
         return x + y;
     });
 }
 console.log(sum([1,4,7,11]));  //23
-*/
+
 //此时如果不想立即返回求和的结果，而是想在以后根据需要求和
 //可以返回这个求和函数
-/*function foo(arr){
+function foo3(arr){
     var sum = function(){
         return arr.reduce(function(x,y){
             return x + y;
@@ -65,14 +65,14 @@ console.log(sum([1,4,7,11]));  //23
     }
     return sum;
 }
-console.log(foo([1,3,5,7,9]));  //返回sum这个函数
-console.log(foo([1,3,5,7,9])());  //返回执行的结果*/
-//在这个例子中，我们在foo()函数中定义了sum函数，并且sum函数可以引用外部函数foo()函数的局部变量和参数。
-//在foo()函数返回的函数sum中，保存了相关参数和变量，这种成为“闭包”
-//注意一点：调用函数foo()时，每次返回的函数sum()都是一个新的函数，即使传入的是相同的参数
+console.log(foo3([1,3,5,7,9]));  //返回sum这个函数
+console.log(foo3([1,3,5,7,9])());  //返回执行的结果
+//在这个例子中，我们在foo3()函数中定义了sum函数，并且sum函数可以引用外部函数foo3()函数的局部变量和参数。
+//在foo3()函数返回的函数sum中，保存了相关参数和变量，这种成为“闭包”
+//注意一点：调用函数foo3()时，每次返回的函数sum()都是一个新的函数，即使传入的是相同的参数
 
 //2.2 --继续闭包
-/*function createFunctions() {
+function createFunctions() {
     var arr = new Array();
     for (var i = 0; i < 5; i++) {
         arr[i] = function () {
@@ -90,7 +90,7 @@ var f3 = result[2];
 //就会去找保存的内部局部变量i，此时循环已经结束，i的值为5
 console.log(f1())    //5
 console.log(f2());    //5
-console.log(f3());    //5*/
+console.log(f3());    //5
 
 //如果想正常返回，就要对函数进行改写
 function createFunctions() {
@@ -130,4 +130,6 @@ console.log(foo1.count());  //11
 console.log(foo1.count());  //12
 console.log(foo1.count());  //13
 
-//函数creatFunctions()
+//函数creat_count()接收一个参数，并且返回一个对象，这个对象引用了外部外部变量x
+//所以当调用creat_count()函数时，传入参数1，执行函数后，返回对象的匿名函数保存着这个变量x的值。
+//下次调用时，依然会在这个基础上加1
