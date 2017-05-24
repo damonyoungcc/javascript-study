3423e18e7e231bcab3256463056db4bf956076d6
yangdepu <965093975@qq.com>
2017-05-24 15:22:49 +0800

    闭包的学习

--- day4/hellojavascript.js
+++ day4/hellojavascript.js
@@ -22,14 +22,112 @@ console.log(arr.filter(function (element, index, array) {
 }));*/
 
 //利用filter()函数可以巧妙的去除数组中重复的元素
-var arr = ['apple', 'xiaomi', 'microsoft', 'IBM', 'apple', 'xiaomi'];
+/*var arr = ['apple', 'xiaomi', 'microsoft', 'IBM', 'apple', 'xiaomi'];
 console.log(arr.filter(function(ele,index,self){
     if(self.indexOf(ele) != index){
         return false;
     }else{
         return true;
     }
-}));
+}));*/
 //indexOf()方法只返回第一次出现要查找的元素的索引
 
 //利用filter()筛选出素数
+
+//test
+//just test值传递
+/*function foo(obj){
+    obj.name = 'yang';
+    obj = new Object();
+    obj.name = 'rose';
+}
+var ob = new Object();
+foo(ob);
+console.log(ob.name);  //yang*/
+
+//2.0 =======================================================
+//2.0 =======================================================
+//2.1 --闭包
+/*function sum(arr){
+    return arr.reduce(function(x,y){
+        return x + y;
+    });
+}
+console.log(sum([1,4,7,11]));  //23
+*/
+//此时如果不想立即返回求和的结果，而是想在以后根据需要求和
+//可以返回这个求和函数
+/*function foo(arr){
+    var sum = function(){
+        return arr.reduce(function(x,y){
+            return x + y;
+        });
+    }
+    return sum;
+}
+console.log(foo([1,3,5,7,9]));  //返回sum这个函数
+console.log(foo([1,3,5,7,9])());  //返回执行的结果*/
+//在这个例子中，我们在foo()函数中定义了sum函数，并且sum函数可以引用外部函数foo()函数的局部变量和参数。
+//在foo()函数返回的函数sum中，保存了相关参数和变量，这种成为“闭包”
+//注意一点：调用函数foo()时，每次返回的函数sum()都是一个新的函数，即使传入的是相同的参数
+
+//2.2 --继续闭包
+/*function createFunctions() {
+    var arr = new Array();
+    for (var i = 0; i < 5; i++) {
+        arr[i] = function () {
+            return i;
+        };
+    }
+    return arr;
+}
+var result = createFunctions();
+//执行creatFuctions()函数，返回的arr数组里面的元素都是匿名函数，匿名函数里面保存了变量i
+var f1 = result[0];
+var f2 = result[1];
+var f3 = result[2];
+//执行函数f1其实指代的是arr数组中的第一个元素匿名函数，后面加()执行函数，
+//就会去找保存的内部局部变量i，此时循环已经结束，i的值为5
+console.log(f1())    //5
+console.log(f2());    //5
+console.log(f3());    //5*/
+
+//如果想正常返回，就要对函数进行改写
+function createFunctions() {
+    var arr = new Array();
+    for (var i = 0; i <= 5; i++) {
+        arr[i] = (function (n) {
+            return i;
+        })(i);    //传入参数i并且立即执行存入arr
+    }
+    return arr;
+}
+var result = createFunctions();
+var f1 = result[0];
+var f2 = result[1];
+var f3 = result[2];
+
+console.log(f1);   //0
+console.log(f2);   //1
+console.log(f3);   //2
+
+//借助闭包实现块级作用域
+function creat_count(init) {
+    var x = 0 || init;
+    return {
+        count: function () {
+            return x = x + 1;
+        }
+    }
+}
+var foo = creat_count(1);
+console.log(foo.count());  //2
+console.log(foo.count());  //3
+console.log(foo.count());  //4
+
+var foo1 = creat_count(10);
+console.log(foo1.count());  //11
+console.log(foo1.count());  //12
+console.log(foo1.count());  //13
+
+//函数creatFunctions()
