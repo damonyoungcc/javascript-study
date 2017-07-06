d96292d3c5483c40eb6c24f5813d92a736d46d90
yangdepu <965093975@qq.com>
2017-07-06 22:36:16 +0800

    jquery学习

---
+++ day7/jquerytest.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+/*    $('div').html('学习慕课网的jquery');
+    $('#imooc').html('通过慕课网学习jquery才是正道').css('color','red');
+    var a1 = $('.foo').get(0);
+    //a1.style.color = 'red';   原生JS操作
+    $(a1).css('color','red');*/
+
+    $('.sscxList.ng-scope p a').css('color','red');
+
+});
