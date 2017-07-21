cac98188da51904eb12d5a7f0bbc8dc5d78a75ca
yangdepu <965093975@qq.com>
2017-07-21 11:42:42 +0800

    node搭建服务器初体验

---
+++ day13/step0/index.js
@@ -0,0 +1,19 @@
+var http = require('http')
+
+var server = http.createServer(function(request, response){
+  //setTimeout(function(){
+    
+    //response.setHeader("Content-Type","text/plain; charset=utf-8")
+    response.setHeader('Content-Type','text/html,charset=utf-8')
+    response.writeHead(202, 'haha')
+    response.write('<html><head><meta charset="utf-8" /></head>')
+    response.write('<body>')
+    response.write('<h1>你好</h1>')
+    response.write('</body>')
+    
+    response.end()
+  //},2000);
+})
+
+console.log('open http://localhost:8080')
+server.listen(8080)
---
+++ day13/step1/server.js
@@ -0,0 +1,47 @@
+var http = require('http')
+var path = require('path')
+var fs = require('fs')
+var url = require('url')
+
+
+function staticRoot(staticPath, req, res){
+  
+  var pathObj = url.parse(req.url, true)
+  
+  //兼容处理
+/*  if(pathObj.pathname === '/'){
+    pathObj.pathname += 'index.html'
+  }*/
+  
+
+  var filePath = path.join(staticPath, pathObj.pathname)
+  
+/*  var fileContent = fs.readFileSync(filePath,'binary')
+  res.write(fileContent, 'binary')
+  res.end()*/
+  
+  
+  fs.readFile(filePath, 'binary', function(err, fileContent){
+    if(err){
+      console.log('404')
+      res.writeHead(404, 'not found')
+      res.end('<h1>404 Not Found</h1>')
+    }else{
+      console.log('ok')
+      res.write(fileContent, 'binary')
+      res.end()      
+    }
+  })
+  
+
+}
+
+var server = http.createServer(function(req, res){
+  staticRoot(path.resolve(__dirname, 'static'), req, res)
+})
+
+server.listen(8080)
+console.log('visit http://localhost:8080' )
+
+
+
