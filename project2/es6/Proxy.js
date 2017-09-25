4b7a983f2ee7ab35b6f705afb262266a27ad6e5d
yangdepp <965093975@qq.com>
2017-09-25 16:15:52 +0800

    布局复习

--- project2/es6/Proxy.js
+++ project2/es6/Proxy.js
@@ -9,4 +9,18 @@ function foo(num) {
         }
     }
 }
-foo(27);
+// foo(1357911);
+
+//参数运算符
+
+
+function sum(...rest) {
+    return rest.reduce(function (x, y) {
+        return x + y;
+    })
+}
+console.log(sum(1, 2, 3, 4, 5, 6))
+
+console.log([1, 2, 3, 4, 5, 6, 7].reduce((x, y) => {
+    return x + y;
+}))
